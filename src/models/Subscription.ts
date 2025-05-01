8d737f596f5eaf417699c17c23bd13474a678214
palthol <ilikericeat@gmail.com>
2025-05-01 14:57:33 -0400

    refactored model import statements

--- src/models/Subscription.ts
+++ src/models/Subscription.ts
@@ -1,5 +1,5 @@
 import { Model, DataTypes } from 'sequelize';
-import sequelize from '../config/database';
+import { sequelize } from '../config/database';
 
 class Subscription extends Model {
   public id!: number;
