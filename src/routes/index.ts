69e47e31b84f9eb32dde3923167de0f935cdef8b
palthol <ilikericeat@gmail.com>
2025-05-21 03:36:23 -0400

    added a barrel file for routes

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import clientRoutes from './clientRoutes';
+import employeeRoutes from './employeeRoutes';
+import subscriptionRoutes from './subscriptionRoutes';
+import trainingRoutes from './trainingRoutes';
+import paymentRoutes from './paymentRoutes';
+
+export {
+  clientRoutes,
+  employeeRoutes,
+  subscriptionRoutes,
+  trainingRoutes,
+  paymentRoutes
+};
